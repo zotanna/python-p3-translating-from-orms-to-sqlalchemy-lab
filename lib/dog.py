dbf56b90da7ffbc5fe4b7827d824048a966b9ccf
Ben Botsford <ben.botsford@flatironschool.com>
2022-08-04 11:44:11 -0500

    remove solution

--- lib/dog.py
+++ lib/dog.py
@@ -1,32 +1,27 @@
 from sqlalchemy import create_engine
 
-from lib.models import Dog
-
 engine = create_engine('sqlite:///:memory:')
 
 def create_table(base):
-    base.metadata.create_all(engine)
-    return engine
+    pass
 
 def save(session, dog):
-    session.add(dog)
-    session.commit()
-    return session
+    pass
 
 def new_from_db(session):
-    return session.query(Dog).first()
+    pass
 
 def get_all(session):
-    return [dog for dog in session.query(Dog)]
+    pass
 
 def find_by_name(session, name):
-    return session.query(Dog).filter_by(dog_name=name).first()
+    pass
 
 def find_by_id(session, id):
-    return session.query(Dog).filter_by(dog_id=id).first()
+    pass
 
 def find_by_name_and_breed(session, name, breed):
-    return session.query(Dog).filter_by(dog_name=name, dog_breed=breed).first()
+    pass
 
 def update(session, dog):
-    session.commit()
+    pass
--- lib/models.py
+++ lib/models.py
@@ -6,9 +6,4 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Dog(Base):
-    __tablename__ = 'dogs'
-    __table_args__ = (PrimaryKeyConstraint('dog_id'),)
-
-    dog_id = Column(Integer())
-    dog_name = Column(String())
-    dog_breed = Column(String())
+    pass

06052006fd2bbe9319d9372b19f31341de48f6fb
ianstrom <iankstrom@gmail.com>
2023-03-21 10:28:34 -0400

    Removed new_from_db function, updated README file to be more clear for environment setup

--- lib/models.py
+++ lib/models.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
+from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Dog(Base):
-    pass
+    __tablename__ = 'dogs'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    breed = Column(String())
